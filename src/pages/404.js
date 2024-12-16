1cfd3def296995f2b2e85ea61c8db1912428bcb5
Jonas Frank <jonas@jkfrank.dk>
2024-12-16 16:22:45 +0100

    routes and pages

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+        <h2>This is not the page you are looking for</h2>
+    </div>
+  )
+}
