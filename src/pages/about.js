873030b87b9889cf1017410d942b34cde9f5422a
Jonas Frank <jonas@jkfrank.dk>
2024-12-16 16:13:38 +0100

    nu med underfolder

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+    <h1>About page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius soluta rerum ab culpa necessitatibus quisquam provident! Dignissimos unde alias illo pariatur et reiciendis, similique ea mollitia ratione autem, sunt voluptates.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius soluta rerum ab culpa necessitatibus quisquam provident! Dignissimos unde alias illo pariatur et reiciendis, similique ea mollitia ratione autem, sunt voluptates.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius soluta rerum ab culpa necessitatibus quisquam provident! Dignissimos unde alias illo pariatur et reiciendis, similique ea mollitia ratione autem, sunt voluptates.</p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import * as React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <section>
+    <div>
+      <h2>Developer</h2>
+      <h3>UX and web designer</h3>
+      <p>Based in Ruds Vedby</p>
+    </div>
+  </section>
 }
