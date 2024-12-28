b25b729f9e7ae1b45d82f0421ded0fdbbb8fafa4
Jonas Frank <jonas@jkfrank.dk>
2024-12-28 16:22:09 +0100

    filer fra development

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,14 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/jonasfrank/Web development/gatsby-revisited/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/jonasfrank/Web development/gatsby-revisited/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/Users/jonasfrank/Web development/gatsby-revisited/src/pages/about.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/jonasfrank/Web development/gatsby-revisited/src/pages/index.js")),
+  "component---src-pages-projects-index-js": preferDefault(require("/Users/jonasfrank/Web development/gatsby-revisited/src/pages/projects/index.js")),
+  "component---src-templates-project-details-js": preferDefault(require("/Users/jonasfrank/Web development/gatsby-revisited/src/templates/project-details.js"))
+}
+
